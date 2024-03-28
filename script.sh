41cc67df7970c1c3054e123417d2287b55918b38
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-03-28 22:31:02 +0200

    Make changes to the script.sh

--- script.sh
+++ script.sh
@@ -13,4 +13,6 @@ echo "New file in a new branch" > new_file.txt
 git add new_file.txt
 git commit -m "Added new file new_file.txt in a new branch"
 
+echo "Adding a colleague as a reviewer and creating a Pull Request..."
+gh pr create --title "Changes in the file" --body "Please review and merge the changes." --base main --head new_branch --reviewer darkravchuk
 echo "Execution completed!"
